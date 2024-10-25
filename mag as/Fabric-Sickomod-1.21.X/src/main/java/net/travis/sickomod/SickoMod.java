b9fac22d47f097013e7971d47ff75e664aaddf49
Satorika69 <vanajroman70@gmail.com>
2024-10-25 06:56:20 +0200

    Mucho Importante

---
+++ mag as/Fabric-Sickomod-1.21.X/src/main/java/net/travis/sickomod/SickoMod.java
@@ -0,0 +1,16 @@
+package net.travis.sickomod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SickoMod implements ModInitializer {
+	public static final String MOD_ID = "sickomod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ mag as/Fabric-Sickomod-1.21.X/src/main/java/net/travis/sickomod/SickoModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.travis.sickomod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SickoModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
